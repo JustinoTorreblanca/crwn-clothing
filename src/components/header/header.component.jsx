1e5aa7042307a9bfc505f3583b4f7bed6ddca3bd
Justino Torreblanca <tinotr95@gmail.com>
2021-02-18 17:44:33 -0600

    added cartIcon and cartDropdown components to our app

--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -4,12 +4,14 @@ import {connect} from 'react-redux';
 import {Link} from 'react-router-dom';
 
 import { auth } from '../../firebase/firebase.utils';
+import CartIcon from '../cart-icon/cart-icon.component';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 import {ReactComponent as Logo } from '../../assets/crown.svg';
 
 import './header.styles.scss';
 
-const Header = ({ currentUser }) => (
+const Header = ({ currentUser, hidden }) => (
   <div className='header'>
     <Link className='logo-container' to="/">
       <Logo className='logo' />
@@ -21,22 +23,25 @@ const Header = ({ currentUser }) => (
       <Link className='option' to='/shop'>
         CONTACT
       </Link>
-      {
-        currentUser ?
+      {currentUser ? (
         <div className='option' onClick={() => auth.signOut()}>
           SIGN OUT
         </div>
-        :
+       ) : (
         <Link className='option' to='/signin'>
           SIGN IN
         </Link>
-      }
+       )}
+       <CartIcon />
     </div>
+    {hidden ? null : <CartDropdown />}
+
   </div>
   );
-
-const mapStateToProps = state => ({
-  currentUser: state.user.currentUser
-})
+//vvv destructure we want the user and of the user qwe want te curren USer, this is the syntax when you want to destructure nested values. en otra palabras dame {este valor} of the user, que al mismo tiempo esta siendo deconstruido del state//
+const mapStateToProps = ({user: { currentUser },cart: { hidden }}) => ({
+  currentUser,
+  hidden
+});
 
 export default connect(mapStateToProps)(Header);
